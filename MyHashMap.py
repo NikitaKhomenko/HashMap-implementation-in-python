a5830bc5165f4c48c1db3aca71d9dc1865134967
nikita <nickita1@gmail.com>
2018-10-23 07:07:30 +0300

    Refactor - methods start with capital

--- MyHashMap.py
+++ MyHashMap.py
@@ -12,7 +12,7 @@ class MyHashMap(object):
             self.capacity = MAXIMUM_CAPACITY
 
         else:
-            self.capacity = self.trimToPowerOf2(capacity)
+            self.capacity = self.TrimToPowerOf2(capacity)
 
         self.thresholdLoadFactor = loadFactor
         self.size = 0
@@ -23,124 +23,124 @@ class MyHashMap(object):
             self.key = key
             self.val = val
 
-            def getKey(self):
+            def get_key(self):
                 return self.key
 
-            def getVal(self):
+            def get_val(self):
                 return self.val
 
-            def setVal(self, val):
+            def set_val(self, val):
                 self.val = val
 
             def __str__(self):
                 return "(" + self.key + ": " + str(self.val) + ")"
 
-    def trimToPowerOf2(self, initialCapacity):  # trims the capacity to power of 2
+    def TrimToPowerOf2(self, initialCapacity):  # trims the capacity to power of 2
         capacity = 1
         while capacity < initialCapacity:
             capacity <<= 1
 
         return capacity
 
-    def hashIt(self, key):
+    def HashIt(self, key):
         hash = 0
         for char in str(key):
             hash += ord(char)
         return hash % self.capacity
 
-    def clear(self):
+    def Clear(self):
         self.size = 0
-        self.removeEntries()
+        self.RemoveEntries()
 
-    def removeEntries(self):
+    def RemoveEntries(self):
         for entry in self.table:
             if entry is not None:
                 entry = None
 
-    def containsKey(self, key):
-        index = self.hashIt(key)
+    def ContainsKey(self, key):
+        index = self.HashIt(key)
         if (self.table[index] is not None) & (self.table[index].getKey().equals(key)):
             return True
         return False
 
-    def containsValue(self, value):
+    def ContainsValue(self, value):
         for entry in self.table:
             if entry is not None:
                 if entry.getValue().equals(value):
                     return True
         return False
 
-    def put(self, key, value):  # adding an element to the map by specified key
-        index = self.hashIt(key)
-        if (self.get(key) is not None) & (self.table[index].getKey().equals(key)):  # if the key already exists
+    def Put(self, key, value):  # adding an element to the map by specified key
+        index = self.HashIt(key)
+        if (self.Get(key) is not None) & (self.table[index].getKey().equals(key)):  # if the key already exists
             oldValue = self.table[index].getValue()
             self.table[index].setValue(value)
             return oldValue
 
-        if (self.size + 1 >= self.capacity * self.thresholdLoadFactor) | (self.get(key) is not None):  # if need rehash
+        if (self.size + 1 >= self.capacity * self.thresholdLoadFactor) | (self.Get(key) is not None):  # if need rehash
             if self.capacity == MAXIMUM_CAPACITY:
                 RuntimeError("Exceeding maximum capacity")
-            self.rehash()
+            self.Rehash()
 
-        newIndex = self.hashIt(key)
+        newIndex = self.HashIt(key)
         self.table[newIndex] = self.Entry(key, value)
         self.size = + 1
         return None
 
-    def rehash(self):  # the rehash function
-        h_set = self.entrySet()
+    def Rehash(self):  # the rehash function
+        h_set = self.EntrySet()
         self.capacity <<= 1
         self.table = self.Entry[self.capacity]
         self.size = 0
         for entry in h_set:
-            self.put(entry.getKey(), entry.getValue())
+            self.Put(entry.getKey(), entry.getValue())
 
-    def entrySet(self):  # return set of the entries(BandEntries) in the map
+    def EntrySet(self):  # return set of the entries(BandEntries) in the map
         h_set = set()
         for entry in self.table:
             if entry is not None:
                 h_set.add(entry)
         return h_set
 
-    def get(self, key):  # returning an element by specified key
+    def Get(self, key):  # returning an element by specified key
         if key is not None:
-            index = self.hashIt(key)
+            index = self.HashIt(key)
             if self.table is not None:
                 if self.table[index] is not None:
                     return self.table[index].getValue()
         return None
 
-    def isEmpty(self):  # returning if the map contains values or not
+    def IsEmpty(self):  # returning if the map contains values or not
         return self.size == 0
 
-    def keySet(self):  # returning a set of the keys in this map
+    def KeySet(self):  # returning a set of the keys in this map
         k_set = set()
-        for entry in self.entrySet():
+        for entry in self.EntrySet():
             k_set.add(entry.getKey())
         return k_set
 
-    def putAll(self, map):  # adding a full map to this map
-        m_set = map.entrySet()
+    def PutAll(self, map):  # adding a full map to this map
+        m_set = map.EntrySet()
 
         for self.entry in m_set:
-            self.put(self.entry.getKey(), self.entry.getValue())
+            self.Put(self.entry.getKey(), self.entry.getValue())
 
-    def remove(self, key):  # removing element by specified key
-        if self.get(key) is None:
+    def Remove(self, key):  # removing element by specified key
+        if self.Get(key) is None:
             return None
 
-        index = self.hashIt(key)
+        index = self.HashIt(key)
         oldValue = self.table[index].getValue()
         self.table[index] = None
         self.size -= 1
         return oldValue
 
-    def size(self):  # retutn size of the map
+    def Size(self):  # retutn size of the map
         return self.size
 
-    def values(self):  # return a set consisting of the values in the map
+    def Values(self):  # return a set consisting of the values in the map
         v_list = list()
-        for entry in self.entrySet():
+        for entry in self.EntrySet():
             v_list.append(entry.getValue())
         return v_list
 
